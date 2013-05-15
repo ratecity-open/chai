a920006b002b6711b76cb79f7b74b0c4d8b3648e
Rob Sharp <qnm@fea.st>
2013-05-16 08:00:07 +1000

    Add spec/support/blueprints and link in via spec_helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,6 +4,11 @@
 # loaded once.
 #
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
+
+# Requires supporting ruby files with custom matchers and macros, etc,
+# in spec/support/ and its subdirectories.
+Dir[Rails.root.join("spec/support/**/*.rb")].each {|f| require f}
+
 RSpec.configure do |config|
   config.treat_symbols_as_metadata_keys_with_true_values = true
   config.run_all_when_everything_filtered = true
