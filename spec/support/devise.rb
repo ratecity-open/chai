2a50f748fb50ae081a2c0b514a5f9cc802ad3b08
Rob Sharp <qnm@fea.st>
2013-05-16 10:44:17 +1000

    Remove code copy/pasta'd from our other app

--- spec/support/devise.rb
+++ spec/support/devise.rb
@@ -2,9 +2,6 @@ require 'devise'
 
 RSpec.configure do |config|
   config.include Devise::TestHelpers, :type => :controller
-  require './spec/support/test_helpers'
-  config.include SorbetApplication::TestHelpers, :type => :request
-
   include Warden::Test::Helpers
   Warden.test_mode!
 end
